2369c180ae28f5adc750d75c4e97fdc33c2f0ebf
Egor Gutsalov <egor.gutsalov@gmail.com>
2024-09-25 23:53:26 +0300

    added gtest

--- lab1/include/join.h
+++ lab1/include/join.h
@@ -3,6 +3,35 @@
 #include "list.h"
 #include <stdlib.h>
 
-char* join(List*); 
+
+static size_t countLen(List* list)
+{
+    List* ptr = list;
+    size_t res = 0;
+    while (ptr != 0)
+    {
+        res += ptr->len;
+        ptr = ptr->next;
+    }
+    return res;
+}
+
+
+
+char* join(List* list)
+{
+    size_t len = countLen(list);
+    char* str = malloc(len + 1);
+    int pos = 0;
+    List* ptr = list;
+    while (ptr != 0)
+    {
+        memcpy((str + pos), ptr->str, ptr->len);
+        pos += ptr->len;
+        ptr = ptr->next;
+    }
+    str[pos] = '\0';
+    return str;
+}
 
 #endif
--- lab1/include/list.h
+++ lab1/include/list.h
@@ -10,10 +10,57 @@ typedef struct List {
     struct List *next;
 } List;
 
-List* initList();
-void createNode(struct List*, const char*);
-void addToList(struct List*, const char*);
-void deleteList(List*);
-void printList(List*);
+
+List* initList()
+{
+    struct List* list = malloc(sizeof(struct List));
+    if(list == NULL) 
+        return NULL;
+    list->len = 0;
+    list->next = 0;
+    list->str = 0;
+    return list;
+}
+
+
+void createNode(struct List* list, const char* str)
+{
+    list->len = strlen(str);
+    list->next = 0;
+    list->str = malloc(list->len);
+    strcpy(list->str, str, list->len);
+}
+
+
+void addToList(struct List* list, const char* str)
+{
+    if(list->str == 0) createNode(list, str);
+    else if(list->next == 0) 
+    {
+        list->next = initList();
+        createNode(list->next, str);
+    }
+    else addToList(list->next, str);
+}
+    
+
+void deleteList(List* list)
+{
+    if(list->next == 0) free(list);
+    else
+    {
+        deleteList(list->next);
+        free(list);
+    }
+}
+
+
+void printList(List* list)
+{
+    printf(list->str);
+    printf("\n");
+    if(list->next != 0) printList(list->next);
+}
+
 
 #endif 
