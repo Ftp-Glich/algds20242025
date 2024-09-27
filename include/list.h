42a6978124268b8982164e58549428ef7539c873
Ftp-Glich <85408465+Ftp-Glich@users.noreply.github.com>
2024-09-28 00:08:52 +0300

    added tests

--- include/list.h
+++ include/list.h
@@ -11,56 +11,19 @@ typedef struct List {
 } List;
 
 
-List* initList()
-{
-    struct List* list = (List*)malloc(sizeof(struct List));
-    if(list == NULL) 
-        return NULL;
-    list->len = 0;
-    list->next = 0;
-    list->str = 0;
-    return list;
-}
+List* initList();
 
 
-void createNode(struct List* list, const char* str)
-{
-    list->len = strlen(str);
-    list->next = 0;
-    list->str = (char*)malloc(list->len);
-    strcpy(list->str, str);
-}
+int createNode(struct List*, const char*);
 
 
-void addToList(struct List* list, const char* str)
-{
-    if(list->str == 0) createNode(list, str);
-    else if(list->next == 0) 
-    {
-        list->next = initList();
-        createNode(list->next, str);
-    }
-    else addToList(list->next, str);
-}
+int addToList(struct List*, const char*);
     
 
-void deleteList(List* list)
-{
-    if(list->next == 0) free(list);
-    else
-    {
-        deleteList(list->next);
-        free(list);
-    }
-}
+void deleteList(List*);
 
 
-void printList(List* list)
-{
-    printf(list->str);
-    printf("\n");
-    if(list->next != 0) printList(list->next);
-}
+void printList(List*);
 
 
 #endif 
--- test/join.cpp
+++ test/join.cpp
@@ -2,6 +2,15 @@
 
 extern "C"
 {
-    #include "lab1/include/join.h"
+    #include "join.h"
+    #include "list.h"
 }
 
+TEST(join_check, ok)
+{
+    List* list = initList();
+    addToList(list, "AsjdfhjashdjasjkdhaS");
+    addToList(list, "AsjdfhjashdjasjkdhaS");
+    addToList(list, "AsjdfhjashdjasjkdhaS");
+    ASSERT_EQ(join(list), "AsjdfhjashdjasjkdhaSAsjdfhjashdjasjkdhaSAsjdfhjashdjasjkdhaS");
+}
